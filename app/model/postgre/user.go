49a5602b3be59ad2c69938e7f3de3a4954fd2caf
Lutfino Athallah Pratama <finolut@gmail.com>
2025-12-14 10:07:51 +0700

    menambah passwordhass di model

--- app/model/postgre/user.go
+++ app/model/postgre/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID           string    `db:"id" json:"id"` // uuid string
 	Username     string    `db:"username" json:"username"`
 	Email        string    `db:"email" json:"email"`
-	PasswordHash string    `db:"password_hash" json:"-"`
+	PasswordHash string    `db:"password_hash" json:"password_hash"`
 	FullName     string    `db:"full_name" json:"full_name"`
 	RoleID       string    `db:"role_id" json:"role_id"` // FK -> roles.id
 	IsActive     bool      `db:"is_active" json:"is_active"`
